b47cc836d632a2357de8530e8e125fc31632d47e
weshford <silvanbelten@gmail.com>
2024-10-09 16:32:35 +0200

    dan du unglaublicher hs
    ich hasse es wenn man es nicht gescheit schafft javascript etc zu formattieren hier war zwar viel jetzt nur apostroph & gänsefußschen
    aber trotzdem

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 /*global minify:false, JS_Parse_Error:false */
 /*jshint globalstrict:true */
 
-'use strict';
+/*
+* wrapper created by Dan Wolff (danwolff.se)
+* this wrapper is in the public domain. attribution not required but appreciated.
+* changes made to wrapper done by yungztrunks
+
+* UglifyJS is released under the BSD license, copyright Mihai Bazon.
+* For more information, see https://github.com/mishoo/UglifyJS2/.
+* 
+* bookmarklet created by yungztrunks (yungztrunks.de)
+*/
+
+"use strict";
 
 var default_options = {};
 
@@ -9,26 +20,26 @@ function $(id) {
 	return document.getElementById(id);
 }
 
-
 // Handle the UI
 
 var uglify_options;
-var $options = $('options');
-var $out = $('out');
-var $in = $('in');
-var $error = $('error');
-var $stats = $('stats');
+var $options = $("options");
+var $out = $("out");
+var $in = $("in");
+var $error = $("error");
+var $stats = $("stats");
 var $body = document.body;
-var $btn_options = $('btn-options');
-var $btn_options_save = $('btn-options-save');
-var $cb_as_i_type = $('cb-as-i-type');
+var $btn_options = $("btn-options");
+var $btn_options_save = $("btn-options-save");
+var $cb_as_i_type = $("cb-as-i-type");
+$("header-link").onclick = go_to_start;
+$("btn-go").onclick = go;
 
+$("btn-gen-bookmarklet").onclick = generate_bookmarklet;
 
-$('header-link').onclick = go_to_start;
-$('btn-go').onclick = go;
 $btn_options.onclick = show_options;
 $btn_options_save.onclick = set_options;
-$('btn-options-reset').onclick = reset_options;
+$("btn-options-reset").onclick = reset_options;
 $in.oninput = $in.onkeyup = $in.onblur = $in.onfocus = go_ait;
 $cb_as_i_type.onclick = set_options_ait;
 $out.onfocus = select_text;
@@ -36,31 +47,30 @@ $out.onfocus = select_text;
 var default_options_text;
 set_options_initial();
 
-
 function hide(class_name) {
-	var names = class_name.split(' ');
-	var cur = ' ' + $body.className + ' ';
+	var names = class_name.split(" ");
+	var cur = " " + $body.className + " ";
 	for (var i = 0; i < names.length; i++) {
-		while (cur.indexOf(' ' + names[i] + ' ') >= 0) {
-			cur = cur.replace(' ' + names[i] + ' ', ' ');
+		while (cur.indexOf(" " + names[i] + " ") >= 0) {
+			cur = cur.replace(" " + names[i] + " ", " ");
 		}
 	}
 
-	$body.className = cur.replace(/^\s+|\s+$/g, '');
+	$body.className = cur.replace(/^\s+|\s+$/g, "");
 }
 
 function show(class_name) {
-	$body.className += ' ' + class_name;
+	$body.className += " " + class_name;
 }
 
 function show_options() {
-	show('s-options');
-	hide('s-input');
+	show("s-options");
+	hide("s-input");
 }
 
 function get_options(value) {
 	/*jshint evil:true */
-	return new Function('return (' + (value || $options.value) + ');')();
+	return new Function("return (" + (value || $options.value) + ");")();
 }
 
 function set_options() {
@@ -71,16 +81,15 @@ function set_options() {
 		// The options could be parsed. Try to update localStorage.
 		try {
 			if (default_options_text === $options.value)
-				localStorage.removeItem('uglify-options');
-			else
-				localStorage.setItem('uglify-options', $options.value);
-		} catch (e) {}
+				localStorage.removeItem("uglify-options");
+			else localStorage.setItem("uglify-options", $options.value);
+		} catch (e) { }
 
 		// Run Uglify with the new options.
 		go(true);
 
-		show('s-input');
-		hide('s-options');
+		show("s-input");
+		hide("s-options");
 		return true;
 	} catch (e) {
 		if (e instanceof JS_Parse_Error) {
@@ -103,10 +112,9 @@ function reset_options() {
 function set_options_ait() {
 	try {
 		if ($cb_as_i_type.checked)
-			localStorage.removeItem('uglify-options-disable-ait');
-		else
-			localStorage.setItem('uglify-options-disable-ait', 1);
-	} catch (e) {}
+			localStorage.removeItem("uglify-options-disable-ait");
+		else localStorage.setItem("uglify-options-disable-ait", 1);
+	} catch (e) { }
 }
 
 function set_options_initial() {
@@ -115,12 +123,12 @@ function set_options_initial() {
 
 	// If there are options saved with localStorage, load them now.
 	try {
-		var options_text = localStorage.getItem('uglify-options');
+		var options_text = localStorage.getItem("uglify-options");
 		if (options_text) {
 			$options.value = options_text;
 		}
-		$cb_as_i_type.checked = !localStorage.getItem('uglify-options-disable-ait');
-	} catch (e) {}
+		$cb_as_i_type.checked = !localStorage.getItem("uglify-options-disable-ait");
+	} catch (e) { }
 
 	try {
 		uglify_options = get_options();
@@ -132,10 +140,10 @@ function set_options_initial() {
 }
 
 function encodeHTML(str) {
-	return (str + '')
-		.replace(/&/g, '&amp;')
-		.replace(/</g, '&lt;')
-		.replace(/"/g, '&quot;');
+	return (str + "")
+		.replace(/&/g, "&amp;")
+		.replace(/</g, "&lt;")
+		.replace(/"/g, "&quot;");
 }
 
 var last_input;
@@ -164,11 +172,15 @@ function go(throw_on_error) {
 			throw res.error;
 		}
 
-		hide('s-info s-error');
-		show('s-output');
+		hide("s-info s-error");
+		show("s-output");
 
-		$out.value = res.code || '/* no output! */';
-		$stats.innerHTML = res.code.length + ' bytes, saved ' + ((1 - res.code.length / input.length) * 100 || 0).toFixed(2) + '%';
+		$out.value = res.code || "/* no output! */";
+		$stats.innerHTML =
+			res.code.length +
+			" bytes, saved " +
+			((1 - res.code.length / input.length) * 100 || 0).toFixed(2) +
+			"%";
 	}
 }
 
@@ -176,12 +188,10 @@ function go(throw_on_error) {
 var ait_timeout;
 var ait_last_duration = 50;
 function go_ait() {
-	if (!$cb_as_i_type.checked)
-		return;
+	if (!$cb_as_i_type.checked) return;
 
 	var input = $in.value;
-	if (input === last_input)
-		return;
+	if (input === last_input) return;
 
 	last_input = input;
 	clearTimeout(ait_timeout);
@@ -193,26 +203,39 @@ function go_ait() {
 }
 
 function show_error(e, param) {
-	console.error('Error', e);
-	hide('s-info s-output');
-	show('s-error');
+	console.error("Error", e);
+	hide("s-info s-output");
+	show("s-error");
 
 	if (e instanceof JS_Parse_Error) {
 		var input = param;
-		var lines = input.split('\n');
+		var lines = input.split("\n");
 		var line = lines[e.line - 1];
-		e = 'Parse error: <strong>' + encodeHTML(e.message) + '</strong>\n' +
-			'<small>Line ' + e.line + ', column ' + (e.col + 1) + '</small>\n\n' +
-			(lines[e.line-2] ? (e.line - 1) + ': ' + encodeHTML(lines[e.line-2]) + '\n' : '') +
-			e.line + ': ' +
-				encodeHTML(line.substr(0, e.col)) +
-				'<mark>' + encodeHTML(line.substr(e.col, 1) || ' ') + '</mark>' +
-				encodeHTML(line.substr(e.col + 1)) + '\n' +
-			(lines[e.line] ? (e.line + 1) + ': ' + encodeHTML(lines[e.line]) : '');
+		e =
+			"Parse error: <strong>" +
+			encodeHTML(e.message) +
+			"</strong>\n" +
+			"<small>Line " +
+			e.line +
+			", column " +
+			(e.col + 1) +
+			"</small>\n\n" +
+			(lines[e.line - 2]
+				? e.line - 1 + ": " + encodeHTML(lines[e.line - 2]) + "\n"
+				: "") +
+			e.line +
+			": " +
+			encodeHTML(line.substr(0, e.col)) +
+			"<mark>" +
+			encodeHTML(line.substr(e.col, 1) || " ") +
+			"</mark>" +
+			encodeHTML(line.substr(e.col + 1)) +
+			"\n" +
+			(lines[e.line] ? e.line + 1 + ": " + encodeHTML(lines[e.line]) : "");
 	} else if (e instanceof Error) {
-		e = e.name + ': <strong>' + encodeHTML(e.message) + '</strong>';
+		e = e.name + ": <strong>" + encodeHTML(e.message) + "</strong>";
 	} else {
-		e = '<strong>' + encodeHTML(e) + '</strong>';
+		e = "<strong>" + encodeHTML(e) + "</strong>";
 	}
 
 	$error.innerHTML = e;
@@ -220,8 +243,8 @@ function show_error(e, param) {
 
 function go_to_start() {
 	clearTimeout(ait_timeout);
-	hide('s-options s-error s-output');
-	show('s-input s-info');
+	hide("s-options s-error s-output");
+	show("s-input s-info");
 	return false;
 }
 
@@ -230,7 +253,7 @@ function select_text() {
 	var self = this;
 	self.select();
 
-	self.onmouseup = self.onkeyup = function() {
+	self.onmouseup = self.onkeyup = function () {
 		// Prevent further mouseup intervention
 		self.onmouseup = self.onkeyup = null;
 		self.scrollTop = 0;
